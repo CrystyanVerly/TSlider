89f539656dcdd17d00f04f9320ce6748266bf409
unknown <crystyanknappverly@gmail.com>
2026-08-08 16:20:38 -0300

    add drag mecanical

--- src/Components/Slide.ts
+++ src/Components/Slide.ts
@@ -6,14 +6,79 @@ interface SlideConfig {
 	options?: SlideOptions;
 }
 
-export default class Slide {
-	wrapper: HTMLElement | null;
-	rail: HTMLElement | null;
-	constructor({ wrapper, rail, options = {} }: SlideConfig) {
-		this.wrapper = document.querySelector<HTMLElement>(wrapper);
-		this.rail = document.querySelector<HTMLElement>(rail);
+interface Distance {
+	initial: number;
+	moving: number;
+	final: number;
+}
 
-		if (!this.wrapper || !this.rail)
-			throw new Error(`Wraper or rail not found.`);
+export default class Slide {
+	wrapper: HTMLElement;
+	rail: HTMLElement;
+
+	private distance: Distance = {
+		initial: 0,
+		moving: 0,
+		final: 0,
+	};
+
+	constructor({ wrapper, rail, options = {} }: SlideConfig) {
+		const wrapperElement = document.querySelector<HTMLElement>(wrapper);
+		const railElement = document.querySelector<HTMLElement>(rail);
+
+		if (!wrapperElement || !railElement)
+			throw new Error(`wrapper or rail not found.`);
+
+		this.wrapper = wrapperElement;
+		this.rail = railElement;
+
+		this.binder();
+	}
+
+	init() {
+		this.mainListener();
+		return this;
+	}
+
+	binder() {
+		this.dragStart = this.dragStart.bind(this);
+		this.dragMove = this.dragMove.bind(this);
+		this.dragEnd = this.dragEnd.bind(this);
+	}
+
+	dragStart(e: PointerEvent) {
+		e.preventDefault();
+		this.distance.initial = Math.round(e.clientX);
+		this.wrapper.addEventListener('pointermove', this.dragMove);
+		window.addEventListener('pointerup', this.dragEnd);
+	}
+
+	dragMove({ clientX }: PointerEvent) {
+		const trackedDist = this.trackOnMoving(clientX);
+		this.moveItem(trackedDist, false);
+		console.log(this.distance);
+	}
+
+	dragEnd(e: PointerEvent) {
+		e.preventDefault();
+		this.distance.final = this.distance.moving;
+
+		this.wrapper.removeEventListener('pointermove', this.dragMove);
+		window.removeEventListener('pointerup', this.dragEnd);
+	}
+
+	moveItem(distX: number, transition = true) {
+		this.rail.style.transition = transition ? `transform .3s ease` : 'none';
+		this.rail.style.transform = `translateX(${distX}px)`;
+	}
+
+	trackOnMoving(clientX: number) {
+		const calcDist = Math.round((clientX - this.distance.initial) * 1.1);
+		this.distance.moving = this.distance.final + calcDist;
+		return this.distance.moving;
+	}
+
+	mainListener() {
+		this.wrapper.addEventListener('pointerdown', this.dragStart);
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Slide from './Components/Slide';
+import './style.css';
 
 const slide = new Slide({
 	wrapper: `[data-slide="wrapper"]`,
 	rail: `[data-slide="rail"]`,
 });
+slide.init();
